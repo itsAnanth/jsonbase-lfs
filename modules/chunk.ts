3daf3c8983a3cf4310c165386c93e0b55fe04850
EJBEAN2op <danonymousarmy@gmail.com>
2022-04-02 17:33:05 +0530

    feat: chunker callback

--- modules/chunk.ts
+++ modules/chunk.ts
@@ -1,12 +1,16 @@
-function chunk(buffer: Buffer, maxBytes: number) {
+function chunk(buffer: Buffer, maxBytes: number, callback: (iterations: number) => any = () => {}) {
     if (buffer.byteLength <= maxBytes) return [buffer];
+
+    let total = Math.ceil(buffer.byteLength / maxBytes), iter = 1;
 
     const result: Buffer[] = [];
     while (buffer.length) {
         if (buffer.length < maxBytes) return [...result, buffer.slice(0, buffer.length)];
 
         result.push(buffer.slice(0, maxBytes));
+        iter++;
         buffer = buffer.slice(maxBytes);
+        callback(+(iter * 100 / total).toFixed(1));
     }
 
     return result;
